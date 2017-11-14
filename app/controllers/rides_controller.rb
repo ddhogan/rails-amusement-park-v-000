a277f4b256342bee7043f6976f3ca34fdf41d93c
Donna <donnamcla@gmail.com>
2017-11-13 21:03:35 -0500

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,7 +6,12 @@ class RidesController < ApplicationController
     def create
         @user = current_user
         @ride = Ride.create(ride_params)
-        redirect_to user_path(@ride.user)
+        rejection_reason = @ride.take_ride
+        if rejection_reason
+            redirect_to user_path(@ride.user), notice: rejection_reason
+        else
+            redirect_to user_path(@ride.user), notice: "Thanks for riding the #{@attraction.name}"
+        end
     end
 
     private
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,21 +3,22 @@ class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
 
+
   def take_ride
     #user needs enough tickets AND to be tall enough
     #updates users ticket amount, nausea, and happiness
     if self.user.height < self.attraction.min_height && self.user.tickets < self.attraction.tickets
-      return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+      rejection_reason = "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
     elsif self.user.height < self.attraction.min_height
-      return "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+      rejection_reason = "Sorry. You are not tall enough to ride the #{self.attraction.name}."
     elsif self.user.tickets < self.attraction.tickets
-      return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+      rejection_reason = "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
     else
       new_tickets = self.user.tickets - self.attraction.tickets
       new_nausea = self.user.nausea + self.attraction.nausea_rating
       new_happiness = self.user.happiness + self.attraction.happiness_rating
       self.user.update(tickets: new_tickets, nausea: new_nausea, happiness: new_happiness)
-      return "Thanks for riding the #{self.attraction.name}!"
+      notice = "Thanks for riding the #{self.attraction.name}!"
     end
   end
 end
